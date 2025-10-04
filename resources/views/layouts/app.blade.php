ef5b5ac8aaa2515cbe09c77cbf04f733bf6e1e5d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-04 10:14:28 +0300

    Creating main Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,27 +19,12 @@
     </head>
     <body class="font-sans antialiased">
         <x-banner />
+        @include('layouts.partials.header')
+        <main class="container mx-auto px-5 flex flex-grow">
 
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
+        </main>
+        @include('layouts.partials.footer')
         @stack('modals')
-
         @livewireScripts
     </body>
 </html>
