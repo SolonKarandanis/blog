e681643efa76491493aadf378c4d37232bf66e1a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-09 08:00:31 +0300

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="{{url('icons/favicon.ico')}}" sizes="any">
+        <link rel="icon" href="{{url('icons/favicon.svg')}}" type="image/svg+xml">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
