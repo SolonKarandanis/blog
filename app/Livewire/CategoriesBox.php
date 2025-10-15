afb829919b1349c7dc1cfcfad129e59dda71bab4
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-15 08:42:53 +0300

    Filtering by Category

---
+++ app/Livewire/CategoriesBox.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Category;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class CategoriesBox extends Component
+{
+
+    #[Computed()]
+    public function categories(){
+        return Category::limit(5)->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.categories-box');
+    }
+}
