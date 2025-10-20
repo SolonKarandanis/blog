e1fafc48b1dc8daabad0f74ca6077a3f20f3ba22
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-20 07:56:33 +0300

    Implementing Post comments

--- app/Livewire/PostComments.php
+++ app/Livewire/PostComments.php
@@ -5,14 +5,38 @@ namespace App\Livewire;
 use App\Models\Comment;
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PostComments extends Component
 {
+    use WithPagination;
+
     public Post $post;
+
+    #[Rule('required|min:3|max:200')]
+    public string $comment;
 
     public function mount(Post $post){
         $this->post = $post;
+    }
+
+    public function postComment()
+    {
+        if (auth()->guest()) {
+            return;
+        }
+
+        $this->validateOnly('comment');
+
+        Comment::create([
+            'body' => $this->comment,
+            'user_id' => auth()->id(),
+            'post_id' => $this->post->id,
+        ]);
+
+        $this->reset('comment');
     }
 
     #[Computed()]
--- resources/views/livewire/post-comments.blade.php
+++ resources/views/livewire/post-comments.blade.php
@@ -2,10 +2,12 @@
     <h2 class="text-2xl font-semibold text-gray-900 dark:text-gray-200 mb-5">Discussions</h2>
     @auth
         <textarea
+            wire:model="comment"
             class="w-full rounded-lg p-4 bg-gray-50 dark:bg-gray-600 focus:outline-none text-sm text-gray-900 dark:text-gray-200 border-gray-200 placeholder:text-gray-400"
             cols="30" rows="7"></textarea>
         <button
-            class="mt-3 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
+            wire:click="postComment"
+            class="mt-3 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-700 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
             Post Comment
         </button>
     @else
