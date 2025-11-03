0c3d6b9463479b03bfdd5ecaa8677ac828febba3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-03 07:20:48 +0200

    Adding caching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $featuredPosts = Post::featured()
-            ->published()
-            ->with(['categories'])
-            ->latest('published_at')
-            ->limit(3)
-            ->get();
-        $latestPosts = Post::published()
-            ->with(['categories'])
-            ->latest('published_at')
-            ->limit(9)
-            ->get();
+        $featuredPosts =Cache::remember('featuredPosts', Carbon::now()->addMinutes(30), function () {
+            return Post::featured()
+                ->published()
+                ->with(['categories'])
+                ->latest('published_at')
+                ->limit(3)
+                ->get();
+        });
+        $latestPosts =Cache::remember('latestPosts', Carbon::now()->addMinutes(30), function (){
+            return Post::published()
+                ->with(['categories'])
+                ->latest('published_at')
+                ->limit(9)
+                ->get();
+        });
 
         return view('home',[
             'featuredPosts' => $featuredPosts,
