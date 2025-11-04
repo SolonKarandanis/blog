906a42282d5e10549fac5457a485dea08249319d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-04 07:05:49 +0200

    Adding posts per month widget

--- app/Filament/Resources/Posts/Pages/ListPosts.php
+++ app/Filament/Resources/Posts/Pages/ListPosts.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use App\Filament\Resources\Posts\Widgets\PostsPerMonthChartWidget;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListPosts extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            PostsPerMonthChartWidget::class,
         ];
     }
 }
