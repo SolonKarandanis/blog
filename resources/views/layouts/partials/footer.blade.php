bbeb8b4d665ba06f3ceca51eba85e1c461dc1d5d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-05 08:12:57 +0200

    Adding blade-flags dependency

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,10 @@
 <footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
-    <a class="text-gray-500 hover:text-red-500" href="">About Us</a>
-    <a class="text-gray-500 hover:text-red-500" href="">Help</a>
-    <a class="text-gray-500 hover:text-red-500" href="">Login</a>
-    <a class="text-gray-500 hover:text-red-500" href="">Explore</a>
+    <div class="flex space-x-4">
+
+    </div>
+    <div class="flex space-x-4">
+        <a class="text-gray-500 hover:text-red-500" href="">{{ __('menu.login') }}</a>
+        <a class="text-gray-500 hover:text-red-500" href="">{{ __('menu.profile') }}</a>
+        <a class="text-gray-500 hover:text-red-500" href="">{{ __('menu.blog') }}</a>
+    </div>
 </footer>
