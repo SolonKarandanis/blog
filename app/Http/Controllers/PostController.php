158f38ffda02e7dac1d471fb9471557db2e119a0
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-10 08:10:19 +0200

    Created PostView model and migrations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,24 +4,35 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Post;
+use App\Models\PostView;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('posts.index');
     }
 
-    public function show(string $slug){
+    public function show(string $slug, Request $request){
         $post = Post::query()
             ->where('slug', $slug)
             ->withCount('likes')
             ->withExists(['likes as is_post_liked' => function ($query) {
                 $query->where('user_id', auth()->id());
             }])
-            ->with(['author','categories'])
+            ->with(['author', 'categories'])
             ->firstOrFail();
-        return view('posts.show',[
-            'post'=>$post
+
+        $user= $request->user();
+        PostView::create([
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'user_id' => $user?->id,
+            'post_id' => $post->id,
+        ]);
+        return view('posts.show', [
+            'post' => $post
         ]);
     }
 }
