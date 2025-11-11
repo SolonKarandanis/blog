a90da110a74d57d2c2a0b1d70bc1074c4fae98f2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-11 07:04:53 +0200

    Added PostOverviewWidget

--- app/Filament/Resources/Posts/Pages/ViewPost.php
+++ app/Filament/Resources/Posts/Pages/ViewPost.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use App\Filament\Resources\Posts\Widgets\PostOverviewWidget;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -14,6 +15,13 @@ class ViewPost extends ViewRecord
     {
         return [
             EditAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PostOverviewWidget::class,
         ];
     }
 }
---
+++ app/Filament/Resources/Posts/Widgets/PostOverviewWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Widgets;
+
+use App\Models\Post;
+use App\Models\PostView;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Eloquent\Model;
+
+class PostOverviewWidget extends StatsOverviewWidget
+{
+    protected ?string $heading = 'Post Statistics';
+
+    protected int | string | array $columnSpan = 'full';
+
+    public ?Model $record=null;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('View Count',PostView::query()->where('post_id','=',$this->record->id)->count()),
+            Stat::make('Post Likes',Post::query()->where('post_id','=',$this->record->id)->withCount('likes')->count()),
+        ];
+    }
+}
