8ba5b10cfcc02f87122ab5946dbefb6e4efff577
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-17 06:20:33 +0200

    Fixed missing root element in comment-create.blade.php

--- resources/views/livewire/comment-create.blade.php
+++ resources/views/livewire/comment-create.blade.php
@@ -1,5 +1,6 @@
-@auth
-    <div x-data="{
+<div>
+    @auth
+        <div x-data="{
             focused: {{ $parentComment ? 'true' : 'false' }},
             init() {
                 if (this.isEdit || this.focused)
@@ -10,7 +11,7 @@
                 })
             }
         }">
-        <form wire:submit.prevent="createComment">
+            <form wire:submit.prevent="createComment">
             <textarea
                 x-ref="input"
                 @click="focused = true"
@@ -18,20 +19,21 @@
                 class="w-full rounded-lg p-4 bg-gray-50 dark:bg-gray-600 focus-outline-none text-sm text-gray-900 dark:text-gray-200 border-gray-200 placeholder:text-gray-400"
                 cols="30"
                 :rows="focused ? '7' : '1'"></textarea>
-            <div :class="focused ? '' : 'hidden'">
-                <button
-                    type="submit"
-                    class="mt-3 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-700 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
-                    Post Comment
-                </button>
-                <button @click="focused = false; $wire.dispatch('cancelEditing')"
-                        type="button"
-                        class="mt-3 ml-2 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-700 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
-                    Cancel
-                </button>
-            </div>
-        </form>
-    </div>
-@else
-    <a wire:navigate class="text-yellow-500 underline py-1" href="{{route('login')}}"> Login to Post Comments</a>
-@endauth
+                <div :class="focused ? '' : 'hidden'">
+                    <button
+                        type="submit"
+                        class="mt-3 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-700 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
+                        Post Comment
+                    </button>
+                    <button @click="focused = false; $wire.dispatch('cancelEditing')"
+                            type="button"
+                            class="mt-3 ml-2 inline-flex items-center justify-center h-10 px-4 font-medium tracking-wide text-white transition duration-200 bg-gray-700 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none">
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
+    @else
+        <a wire:navigate class="text-yellow-500 underline py-1" href="{{route('login')}}"> Login to Post Comments</a>
+    @endauth
+</div>
